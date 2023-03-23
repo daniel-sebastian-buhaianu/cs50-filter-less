acc9d6c9e31c250ffb0ed445610399dc49a4d8ef
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-03-23 09:59:02 +0200

    implemented blur and shortened code in helper functions

--- helpers.c
+++ helpers.c
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdarg.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
@@ -62,11 +64,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 	{
 		for (int j = 0; j < width; j++)
 		{
-			int k = width - 1 - j;
-			// mirrored_image[i][j] = image[i][k] to create mirror effect
-			mirrored_image[i][j].rgbtRed = image[i][k].rgbtRed;
-			mirrored_image[i][j].rgbtGreen = image[i][k].rgbtGreen;
-			mirrored_image[i][j].rgbtBlue = image[i][k].rgbtBlue;
+			mirrored_image[i][j] = image[i][width - 1 -j];
 		}
 	}
 
@@ -75,9 +73,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 	{
 		for (int j = 0; j < width; j++)
 		{
-			image[i][j].rgbtRed = mirrored_image[i][j].rgbtRed;
-			image[i][j].rgbtGreen = mirrored_image[i][j].rgbtGreen;
-			image[i][j].rgbtBlue = mirrored_image[i][j].rgbtBlue;
+			image[i][j] = mirrored_image[i][j];
 		}
 	}
 	return;
@@ -86,5 +82,82 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			/*
+				special cases: [0][j], [height - 1][j], [i][0], [i][width -1]
+			*/
+			if (i == 0)
+			{
+				if (j == 0)
+				{
+					image[i][j] = calc_blur_rgb(4, image[i][j], image[i][j + 1], image[i + 1][j], image[i + 1][j + 1]);
+				}
+				else if (j == width - 1)
+				{
+					image[i][j] = calc_blur_rgb(4, image[i][j], image[i][j - 1], image[i + 1][j - 1], image[i + 1][j]);
+				}
+				else
+				{
+					image[i][j] = calc_blur_rgb(6, image[i][j], image[i][j - 1], image[i][j + 1], image[i + 1][j - 1], image[i + 1][j], image[i + 1][j + 1]);
+				}
+			}
+			else if (i == height - 1)
+			{
+				if (j == 0)
+				{
+					image[i][j] = calc_blur_rgb(4, image[i][j], image[i - 1][j], image[i - 1][j + 1], image[i][j + 1]);
+				}
+				else if (j == width - 1)
+				{
+					image[i][j] = calc_blur_rgb(4, image[i][j], image[i][j - 1], image[i - 1][j - 1], image[i - 1][j]);
+				}
+				else
+				{
+					image[i][j] = calc_blur_rgb(6, image[i][j], image[i][j - 1], image[i - 1][j - 1], image[i - 1][j], image[i - 1][j + 1], image[i][j + 1]);
+				}
+			}
+			else if (j == 0)
+			{
+				image[i][j] = calc_blur_rgb(6, image[i][j], image[i - 1][j], image[i - 1][j + 1], image[i][j + 1], image[i + 1][j + 1], image[i + 1][j]);
+			}
+			else if (j == width - 1)
+			{
+				image[i][j] = calc_blur_rgb(6, image[i][j], image[i - 1][j], image[i - 1][j - 1], image[i][j - 1], image[i + 1][j - 1], image[i + 1][j]);
+			}
+			else
+			{
+				image[i][j] = calc_blur_rgb(9, image[i - 1][j - 1], image[i - 1][j], image[i - 1][j +1], image[i][j - 1], image[i][j], image[i][j + 1], image[i + 1][j - 1], image[i + 1][j], image[i + 1][j + 1]);
+
+			}
+		}
+	}
     return;
+}
+
+// Calculate blur rgb values for a pixel based on N neighbours
+RGBTRIPLE calc_blur_rgb(int count, ...)
+{
+	va_list ap;
+	int j;
+	RGBTRIPLE triple, blur_triple;
+	float avg_red = 0.0, avg_green = 0.0, avg_blue = 0.0;
+
+	va_start(ap, count);
+	for (j = 0; j < count; j++)
+	{
+		triple = va_arg(ap, RGBTRIPLE);
+		avg_red += triple.rgbtRed / (float)count;
+		avg_green += triple.rgbtGreen / (float)count;
+		avg_blue += triple.rgbtBlue / (float)count;
+	}
+	va_end(ap);
+
+	blur_triple.rgbtRed = round(avg_red);
+	blur_triple.rgbtGreen = round(avg_green);
+	blur_triple.rgbtBlue = round(avg_blue);
+
+	return blur_triple;
 }
--- helpers.h
+++ helpers.h
@@ -11,3 +11,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]);
+
+// Calculate blur rgb values for image[i][j] based on its neighbours
+RGBTRIPLE calc_blur_rgb(int count, ...);
